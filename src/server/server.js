f9f37fa72ccc0ae7a2978012c664d0a1a60e802a
Alex Egorenkov <aegorenk@gmail.com>
2018-04-10 11:47:39 -0400

    Bring up mongo and mongoose with a test example.

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+// Mongo
+const mongoose = require('mongoose');
+const mongoURI = process.env.NODE_ENV === 'test' ? 'mongodb://localhost/codiphany' : 'mongodb://localhost/codiphanydev';
+mongoose.connect(mongoURI);
+// Helpers
 const path = require('path');
-const app = express();
 const http = require('http');
-
+// App
+const app = express();
 const server = http.createServer(app);
+
+const solution = require('./solution/solutionModel');
 
 app.get('/', (req, res) => {
   return res.sendFile(path.resolve(__dirname,'../../index.html'));
@@ -11,6 +18,25 @@ app.get('/', (req, res) => {
 
 app.get('/build/webpack-bundle.js', (req, res) => {
   return res.sendFile(path.resolve(__dirname,'../../build/webpack-bundle.js'));
+});
+
+app.get('/solutions', (req, res) => {
+  solution.remove({}, (err, result) => {
+    if (err) console.log(err);
+    solution.create({
+      id: '08uh23fu80hnv2',
+      title: 'title',
+      description: 'desc',
+      resources: 'resources',
+      code: 'code',
+    }, (err, result) => {
+      if (err) console.log(err);
+      solution.findOne({id: '08uh23fu80hnv2'}, (err, result) => {
+        if (err) console.log(err);
+        res.json(result);
+      });
+    });
+  });
 });
 
 server.listen(3000, () => {
---
+++ src/server/solution/solutionModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const solutionSchema = new Schema({
+  id: {type: String, required: true, unique: true},
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  resources: {type: String, required: true},
+  code: {type: String, required: true},
+});
+
+// userSchema.methods.compareHash = function (password) {
+//   return bcrypt.compareSync(password, this.password);
+// }
+
+module.exports = mongoose.model('Solution', solutionSchema);
